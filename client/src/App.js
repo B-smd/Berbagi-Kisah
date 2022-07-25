72c537ff669e0b3ece254b7b28caed9ecfdc2433
B-smd <djaja@iinet.net.au>
2022-07-25 22:17:51 +0800

    progress frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import HomePage from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
+import HomePage from './pages/Home';
+import AboutMePage from './pages/AboutMe';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import ContactMe from './pages/ContactMe';
+
 
 function App() {
   return (
-    <>
-    <HomePage/>
-    </>
+    <Router>
+      <Wrapper>
+        <Header/>
+        <Routes>
+          <Route
+            path="/React-Portfolio"
+            element={<HomePage/>}
+          />
+          <Route
+            path="/"
+            element={<HomePage/>}
+          />
+
+          <Route
+            path="/aboutme"
+            element={<AboutMePage/>}
+          />
+          <Route
+            path="/projects"
+            element={<Projects/>}
+          />
+          <Route
+            path="/resume"
+            element={<Resume/>}
+          />
+          <Route
+            path="/contact"
+            element={<ContactMe/>}
+          />
+        </Routes>
+        <Footer/>
+      </Wrapper>
+    </Router>
   );
 }
 
 export default App;
+
