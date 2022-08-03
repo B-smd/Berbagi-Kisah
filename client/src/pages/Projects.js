30dbb1129a89a8a7530f9363b8ea0c3c13a19742
B-smd <djaja@iinet.net.au>
2022-08-03 09:20:11 +0800

    undo

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import ProjectCard from "../components/ProjectCard";
+import Col from "../components/Col";
+import Row from "../components/Row";
+import projectData from "../data/projects.json"
+
+
+class Projects extends Component {
+    state = {
+        projects: projectData
+    }
+
+    render() {
+        return (
+            <Row>
+                {this.state.projects.map( project => (
+                        <Col>
+                            <ProjectCard
+                                name={project.name}
+                                github={project.github}
+                                demo={project.demo}
+                                image={`${process.env.PUBLIC_URL}/${project.image}`}
+                                description={project.description}
+                            />
+                            <hr/>
+                        </Col>
+                ))}
+            </Row>
+        );
+    }
+}
+
+export default Projects
--- client/src/pages/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Hero from '../components/Hero/index.js';
-
-
-export default function WelcomePage() {
-    return <Hero/>
-}
-
