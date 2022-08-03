dcb41c9661c5d067eab53c0950728bcbd3cf7e47
B-smd <djaja@iinet.net.au>
2022-08-03 19:05:30 +0800

    add pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,39 @@
 import React from 'react';
-import Hero from '../components/Hero';
-import Header from '../components/Header';
+import { useQuery } from '@apollo/client';
 
-export default function HomePage() {
-    return <Hero/>
-}
+import StoryList from '../components/StoryList';
+import StoryForm from '../components/StoryForm';
+
+import { QUERY_STORIES } from '../utils/queries';
+
+const Home = () => {
+    const { loading, data, refetch } = useQuery(QUERY_STORIES);
+
+    const stories = data?.stories || [];
+  
+    return (
+      <main>
+        <div className="flex-row justify-center">
+          <div
+            className="col-12 col-md-10 mb-3 p-3"
+            style={{ border: '1px dotted #1a1a1a' }}
+          >
+            <StoryForm />
+          </div>
+          <div className="col-12 col-md-8 mb-3">
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <StoryList
+                stories={stories}
+                refetchStories={refetch}
+                title="Some Feed for Story(s)..."
+              />
+            )}
+          </div>
+        </div>
+      </main>
+    );
+  };
+  
+  export default Home;
--- client/src/pages/Stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Stories from '../components/Stories';
-
-export default function StoriesPage() {
-    return <Stories/>
-}
